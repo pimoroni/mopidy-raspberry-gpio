d5851339a5be61f71c05c57398170c312b479692
wildestpixel <47211094+wildestpixel@users.noreply.github.com>
2020-07-14 12:15:49 +0100

    Update frontend.py
    Add definition of handle_play_stop which will allow the ALSA playback device to unlock for audio to be played back from an alternative source

--- mopidy_raspberry_gpio/frontend.py
+++ mopidy_raspberry_gpio/frontend.py
@@ -62,6 +62,12 @@ class RaspberryGPIOFrontend(pykka.ThreadingActor, core.CoreListener):
             self.core.playback.pause()
         else:
             self.core.playback.play()
+            
+    def handle_play_stop(self, config):
+        if self.core.playback.get_state().get() == core.PlaybackState.PLAYING:
+            self.core.playback.stop()
+        else:
+            self.core.playback.play()
 
     def handle_next(self, config):
         self.core.playback.next()
